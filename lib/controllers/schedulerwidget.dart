687b722443ce25ef2132fa0be9614cafc1a0a670
Poornima <poornima@Poornimas-Air.home>
2022-01-05 01:17:01 +0100

    Part 2: Group list expansion implementation

--- lib/controllers/schedulerwidget.dart
+++ lib/controllers/schedulerwidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutterdemonstration/controllers/schedule.dart';
 import 'package:flutterdemonstration/data/model/medicines.dart';
+import 'package:flutterdemonstration/data/model/schedules.dart';
 import 'package:flutterdemonstration/helpers/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -34,16 +35,37 @@ class _SchedulerWidgetState extends State<SchedulerWidget> {
               child: ListTile(
                 contentPadding: const EdgeInsets.only(
                     left: Dimensions.pX4, right: Dimensions.pX14),
+                onTap: () {
+                  setState(() {
+                    bool isValid = false;
+                    (widget.item.isExpand) ? isValid = false : isValid = true;
+                    widget.item.isExpand = isValid;
+                  });
+                },
                 title: _setCellTitleContent(),
                 subtitle: _setCellSubTitleContent(),
                 trailing: _setCellTrailingImageContent(),
                 leading: _setCellLeadingImageContent(),
               ),
             ),
+            (widget.item.isExpand)
+                ? Column(
+                    children: _buildExpandableContent(widget.item.scheduler))
+                : Column(
+                    children: const <Widget>[],
+                  )
           ],
         ),
       ),
     );
+  }
+
+  _buildExpandableContent(Scheduler medicines) {
+    List<Widget> columnContent = [];
+    for (Medicines content in medicines.medicines) {
+      columnContent.add(_setExpandableContent(content));
+    }
+    return columnContent;
   }
 
   Widget _setCellTitleContent() {
@@ -83,9 +105,34 @@ class _SchedulerWidgetState extends State<SchedulerWidget> {
   Widget _setCellLeadingImageContent() {
     return Container(
       padding: const EdgeInsets.only(
-          left: Dimensions.pX10, top: Dimensions.pX10, bottom: Dimensions.pX10),
+          left: Dimensions.pX10,
+          top: Dimensions.pX10,
+          bottom: Dimensions.pX10),
       child: Image.asset(IconTypes.displayText(widget.item.scheduler.icon),
           fit: BoxFit.fill),
     );
   }
+}
+
+Widget _setExpandableContent(Medicines medicines) {
+  return Card(
+    child: ListTile(
+        title: Text(
+          medicines.name,
+          style: AppTheme().titleBlack,
+        ),
+        subtitle: Text(
+          medicines.dose,
+          style: AppTheme().subTitleBlack,
+        ),
+        trailing: medicines.isTaken
+            ? Image.asset(CheckBoxTypes.displayText(3),
+                height: Dimensions.pX20,
+                width: Dimensions.pX20,
+                fit: BoxFit.fill)
+            : Image.asset(CheckBoxTypes.displayText(1),
+                height: Dimensions.pX20,
+                width: Dimensions.pX20,
+                fit: BoxFit.fill)),
+  );
 }
